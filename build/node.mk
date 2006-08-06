537aacd161ee867f00d757c220c3571efbc706f0
Maarten ter Huurne <maarten@treewalker.org>
2006-08-06 00:04:39 +0000

    Include "package-darwin" dir in distribution archive.
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@121 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/node.mk
+++ build/node.mk
@@ -6,6 +6,7 @@ DIST:= \
 	main.mk version.mk \
 	node-end.mk node-start.mk \
 	config.guess detectsys.sh \
-	install-recursive.sh
+	install-recursive.sh \
+	package-darwin
 
 include build/node-end.mk
