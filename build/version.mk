774c3294e0e57da7acb8123cf41d4df79e76b2fd
Maarten ter Huurne <maarten@treewalker.org>
2006-08-06 00:07:03 +0000

    Keep version info in Makefile, so it can be used in archive names among others. I set the initial version arbitrarily. Edwin: If you prefer another number, just change it.
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@122 7512d703-071c-0410-b5c2-d2bdd895b7d1

---
+++ build/version.mk
@@ -0,0 +1,12 @@
+# $Id$
+#
+# Version info.
+
+# Name used for packaging.
+PACKAGE_NAME:=openmsx-debugger
+
+# Version number.
+PACKAGE_VERSION:=0.1.0
+
+# Is this a release version ("true") or development version ("false").
+RELEASE_FLAG:=false
