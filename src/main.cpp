840489dd8df66d57d43191609c63a246e763d76b
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2006-10-09 20:31:36 +0000

    Added window icon for all platforms; icon created by Eric Boon
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@5766 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include "DebuggerForm.h"
 #include "Settings.h"
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
+	app.setWindowIcon(QIcon::QIcon(":icons/logo.png"));
 	// restore main settings
 	app.setFont( Settings::get().font( Settings::APP_FONT ) );
 	
