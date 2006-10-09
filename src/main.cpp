fc1ddb3c29674409d7d036210e5b976b08f9ac09
Maarten ter Huurne <maarten@treewalker.org>
2006-10-09 21:58:05 +0000

    Do not set window icon on OS X.
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@5767 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,11 @@
 int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
+// Don't set the icon on OS X, because it will replace the high-res version
+// with a lower resolution one, even though logo.png is 128x128.
+#ifndef __APPLE__
 	app.setWindowIcon(QIcon::QIcon(":icons/logo.png"));
+#endif
 	// restore main settings
 	app.setFont( Settings::get().font( Settings::APP_FONT ) );
 	
