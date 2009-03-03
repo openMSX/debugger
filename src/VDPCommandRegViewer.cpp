7deab6951d7271c4acebbe5be79d5158b1816887
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-03-03 17:54:43 +0000

    cleanups WIP, VDPCommandRegViewer
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@9325 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/VDPCommandRegViewer.cpp
+++ src/VDPCommandRegViewer.cpp
@@ -4,40 +4,39 @@ static const int VIEW_HEX = 1;
 static const int VIEW_SEPERATE = 2;
 
 
-
-VDPCommandRegViewer::VDPCommandRegViewer( QWidget *parent)
+VDPCommandRegViewer::VDPCommandRegViewer(QWidget* parent)
 	: QDialog(parent)
 {
 	setupUi(this);
-	regs =  new unsigned char[64+16];
-	memset(regs, 0, 64+16);
+	regs = new unsigned char[64 + 16];
+	memset(regs, 0, 64 + 16);
 	statusregs = regs + 64;
 
 	// create the needed groups
-	grp_sx = new view88to16(lineEdit_r32,lineEdit_r33,lineEdit_sx);
-	connect(lineEdit_r32,SIGNAL(editingFinished()),grp_sx,SLOT(finishRL()));
-	connect(lineEdit_r33,SIGNAL(editingFinished()),grp_sx,SLOT(finishRH()));
-	connect(lineEdit_sx,SIGNAL(editingFinished()),grp_sx,SLOT(finishRW()));
-	grp_sy = new view88to16(lineEdit_r34,lineEdit_r35,lineEdit_sy);
-	connect(lineEdit_r34,SIGNAL(editingFinished()),grp_sy,SLOT(finishRL()));
-	connect(lineEdit_r35,SIGNAL(editingFinished()),grp_sy,SLOT(finishRH()));
-	connect(lineEdit_sy,SIGNAL(editingFinished()),grp_sy,SLOT(finishRW()));
-	grp_dx = new view88to16(lineEdit_r36,lineEdit_r37,lineEdit_dx);
-	connect(lineEdit_r36,SIGNAL(editingFinished()),grp_dx,SLOT(finishRL()));
-	connect(lineEdit_r37,SIGNAL(editingFinished()),grp_dx,SLOT(finishRH()));
-	connect(lineEdit_dx,SIGNAL(editingFinished()),grp_dx,SLOT(finishRW()));
-	grp_dy = new view88to16(lineEdit_r38,lineEdit_r39,lineEdit_dy);
-	connect(lineEdit_r38,SIGNAL(editingFinished()),grp_dy,SLOT(finishRL()));
-	connect(lineEdit_r39,SIGNAL(editingFinished()),grp_dy,SLOT(finishRH()));
-	connect(lineEdit_dy,SIGNAL(editingFinished()),grp_dy,SLOT(finishRW()));
-	grp_nx = new view88to16(lineEdit_r40,lineEdit_r41,lineEdit_nx);
-	connect(lineEdit_r40,SIGNAL(editingFinished()),grp_nx,SLOT(finishRL()));
-	connect(lineEdit_r41,SIGNAL(editingFinished()),grp_nx,SLOT(finishRH()));
-	connect(lineEdit_nx,SIGNAL(editingFinished()),grp_nx,SLOT(finishRW()));
-	grp_ny = new view88to16(lineEdit_r42,lineEdit_r43,lineEdit_ny);
-	connect(lineEdit_r42,SIGNAL(editingFinished()),grp_ny,SLOT(finishRL()));
-	connect(lineEdit_r43,SIGNAL(editingFinished()),grp_ny,SLOT(finishRH()));
-	connect(lineEdit_ny,SIGNAL(editingFinished()),grp_ny,SLOT(finishRW()));
+	grp_sx = new view88to16(lineEdit_r32, lineEdit_r33, lineEdit_sx);
+	connect(lineEdit_r32, SIGNAL(editingFinished()), grp_sx, SLOT(finishRL()));
+	connect(lineEdit_r33, SIGNAL(editingFinished()), grp_sx, SLOT(finishRH()));
+	connect(lineEdit_sx,  SIGNAL(editingFinished()), grp_sx, SLOT(finishRW()));
+	grp_sy = new view88to16(lineEdit_r34, lineEdit_r35, lineEdit_sy);
+	connect(lineEdit_r34, SIGNAL(editingFinished()), grp_sy, SLOT(finishRL()));
+	connect(lineEdit_r35, SIGNAL(editingFinished()), grp_sy, SLOT(finishRH()));
+	connect(lineEdit_sy,  SIGNAL(editingFinished()), grp_sy, SLOT(finishRW()));
+	grp_dx = new view88to16(lineEdit_r36, lineEdit_r37, lineEdit_dx);
+	connect(lineEdit_r36, SIGNAL(editingFinished()), grp_dx, SLOT(finishRL()));
+	connect(lineEdit_r37, SIGNAL(editingFinished()), grp_dx, SLOT(finishRH()));
+	connect(lineEdit_dx,  SIGNAL(editingFinished()), grp_dx, SLOT(finishRW()));
+	grp_dy = new view88to16(lineEdit_r38, lineEdit_r39, lineEdit_dy);
+	connect(lineEdit_r38, SIGNAL(editingFinished()), grp_dy, SLOT(finishRL()));
+	connect(lineEdit_r39, SIGNAL(editingFinished()), grp_dy, SLOT(finishRH()));
+	connect(lineEdit_dy,  SIGNAL(editingFinished()), grp_dy, SLOT(finishRW()));
+	grp_nx = new view88to16(lineEdit_r40, lineEdit_r41, lineEdit_nx);
+	connect(lineEdit_r40, SIGNAL(editingFinished()), grp_nx, SLOT(finishRL()));
+	connect(lineEdit_r41, SIGNAL(editingFinished()), grp_nx, SLOT(finishRH()));
+	connect(lineEdit_nx,  SIGNAL(editingFinished()), grp_nx, SLOT(finishRW()));
+	grp_ny = new view88to16(lineEdit_r42, lineEdit_r43, lineEdit_ny);
+	connect(lineEdit_r42, SIGNAL(editingFinished()), grp_ny, SLOT(finishRL()));
+	connect(lineEdit_r43, SIGNAL(editingFinished()), grp_ny, SLOT(finishRH()));
+	connect(lineEdit_ny,  SIGNAL(editingFinished()), grp_ny, SLOT(finishRW()));
 
 	grp_sx->setRW(QString("0"));
 	grp_sy->setRW(QString("0"));
@@ -46,29 +45,25 @@ VDPCommandRegViewer::VDPCommandRegViewer( QWidget *parent)
 	grp_nx->setRW(QString("0"));
 	grp_ny->setRW(QString("0"));
 
-	grp_l_sx = new view88to16(label_r_32,label_r_33,label_r_sx);
-	grp_l_sy = new view88to16(label_r_34,label_r_35,label_r_sy);
-	grp_l_dx = new view88to16(label_r_36,label_r_37,label_r_dx);
-	grp_l_dy = new view88to16(label_r_38,label_r_39,label_r_dy);
-	grp_l_nx = new view88to16(label_r_40,label_r_41,label_r_nx);
-	grp_l_ny = new view88to16(label_r_42,label_r_43,label_r_ny);
+	grp_l_sx = new view88to16(label_r_32, label_r_33, label_r_sx);
+	grp_l_sy = new view88to16(label_r_34, label_r_35, label_r_sy);
+	grp_l_dx = new view88to16(label_r_36, label_r_37, label_r_dx);
+	grp_l_dy = new view88to16(label_r_38, label_r_39, label_r_dy);
+	grp_l_nx = new view88to16(label_r_40, label_r_41, label_r_nx);
+	grp_l_ny = new view88to16(label_r_42, label_r_43, label_r_ny);
 
 	//Connect the checkboxes
-	QList<QCheckBox*> list;
-
-        // Warning: This function is not available with MSVC 6!! Not that it
-        // matters to me on my Linux environment :-)
-        list = this->findChildren<QCheckBox*>();
-        QCheckBox* item;
-        foreach (item , list){
-                connect( item, SIGNAL( stateChanged(int) ),
-                         this, SLOT( R45BitChanged(int) ) );
-        };
+	QList<QCheckBox*> list = this->findChildren<QCheckBox*>();
+	QCheckBox* item;
+	foreach(item, list) {
+		connect(item, SIGNAL(stateChanged(int)),
+		        this, SLOT(R45BitChanged(int)));
+	}
 
 	lineEdit_r44->setText("0x00");
 	lineEdit_r45->setText("0x00");
 	lineEdit_r46->setText("0x00");
-	R46=0;
+	R46 = 0;
 	comboBox_cmd->setCurrentIndex(0);
 	//decodeR46(R46);
 	//on_comboBox_cmd_currentIndexChanged();
@@ -82,83 +77,97 @@ VDPCommandRegViewer::~VDPCommandRegViewer()
 	delete[] regs;
 }
 
-
 void VDPCommandRegViewer::on_lineEdit_r45_editingFinished()
 {
-	int val=lineEdit_r45->text().toInt(NULL,0);
 	//for now simply recheck all the checkBoxes and recreate R45
-	QList<QCheckBox*> list;
-        list = this->findChildren<QCheckBox*>(); // Warning:  MSVC 6!! 
-        QCheckBox* item;
+	int val = lineEdit_r45->text().toInt(NULL, 0);
 	int r45 = 0;
-        foreach (item , list){
-	        int order= QString(item->objectName().right(1)).toInt();
-		if ( val & (1<<order) ){
-			r45 = r45 | (1<<order);
+	QList<QCheckBox*> list = findChildren<QCheckBox*>();
+	QCheckBox* item;
+	foreach(item, list) {
+	        int order = QString(item->objectName().right(1)).toInt();
+		if (val & (1 << order)) {
+			r45 = r45 | (1 << order);
 			item->setChecked(true);
 		} else {
 			item->setChecked(false);
 		}
-        };
-	lineEdit_r45->setText(QString("0x%1").arg(r45,2,16,QChar('0')).toUpper());
-	label_arg->setText(QString("%1").arg(r45,8,2,QChar('0')));
-
+	}
+	lineEdit_r45->setText(QString("0x%1").arg(r45, 2, 16, QChar('0')).toUpper());
+	label_arg->setText(QString("%1").arg(r45, 8, 2, QChar('0')));
 }
 
-void VDPCommandRegViewer::on_comboBox_cmd_currentIndexChanged ( int index )
+void VDPCommandRegViewer::on_comboBox_cmd_currentIndexChanged(int index)
 {
-	if ((index<7 || index>9) && index!=11 && index!=5 ){
+	if ((index < 7 || index > 9) && index != 11 && index != 5) {
 		comboBox_operator->setEnabled(false);
 		comboBox_operator->setCurrentIndex(5);
 	} else {
-		if (!comboBox_operator->isEnabled()) comboBox_operator->setCurrentIndex(R46 & 15);
+		if (!comboBox_operator->isEnabled()) {
+			comboBox_operator->setCurrentIndex(R46 & 15);
+		}
 		comboBox_operator->setEnabled(true);
-	};
-	R46 = (15 & R46) + 16*index;
+	}
+	R46 = (15 & R46) + 16 * index;
 	decodeR46(R46);
-	lineEdit_r46->setText(QString("0x%1").arg(R46,2,16,QChar('0')).toUpper());
+	lineEdit_r46->setText(QString("0x%1").arg(R46, 2, 16, QChar('0')).toUpper());
 }
 
-void VDPCommandRegViewer::on_comboBox_operator_currentIndexChanged ( int index )
+void VDPCommandRegViewer::on_comboBox_operator_currentIndexChanged(int index)
 {
 	//if the cmd disables the combox then no influence on R45
 	if (!comboBox_operator->isEnabled()) return;
 
 	R46 = index + (R46 & 0xF0);
 	decodeR46(R46);
-	lineEdit_r46->setText(QString("0x%1").arg(R46,2,16,QChar('0')).toUpper());
+	lineEdit_r46->setText(QString("0x%1").arg(R46, 2, 16, QChar('0')).toUpper());
 }
 
-void VDPCommandRegViewer::decodeR46( int val )
+void VDPCommandRegViewer::decodeR46(int val)
 {
-	QString words[] = { "-", "VDP", "VRAM", "CPU", "Byte", "Dot",
-			"Stop","Invalid","Point","Pset","Search","Line","Logical move","High-speed move"};
-	int dest[]={  0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2 }; //refers to words
-	int srce[]={  0, 0, 0, 0, 2, 1, 1, 1, 1, 2, 2, 3, 1, 2, 2, 3 }; //refers to words
-	int data[]={  0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4 }; //refers to words
-	int cnam[]={  6, 7, 7, 7, 8, 9,10,11,12,12,12,12,13,13,13,13 }; //refers to words
-	QString oper[] = {"IMP","AND","OR","EOR","NOT","---","---","---",
-			"TIMP","TAND","TOR","TEOR","TNOT","---","---","---" };
-	int argbold[]={ 0x00,0x00,0x00,0x00,
-			0x10,0x20,0x26,0x2D,
-			0x2C,0x2C,0x1C,0x2C,
-			0x2C,0x3C,0x2C,0x2C };
-	int regbold[]={ 0x00,0x00,0x00,0x00,
-			0x83,0xCC,0xC3,0xFC,
-			0xFC,0xBF,0xB3,0xFC,
-			0xFC,0xBF,0xAE,0xFC };
-	int operat=val&15;
-	int cmd=(val>>4)&15;
+	static const QString words[16] = {
+		"-", "VDP", "VRAM", "CPU", "Byte", "Dot", "Stop", "Invalid",
+		"Point", "Pset", "Search", "Line", "Logical move", "High-speed move"
+	};
+	static const int dest[16] = {
+		0, 0, 0, 0, 1, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2 //refers to words
+	};
+	static const int srce[16] = {
+		0, 0, 0, 0, 2, 1, 1, 1, 1, 2, 2, 3, 1, 2, 2, 3 //refers to words
+	};
+	static const int data[16] = {
+		0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4 //refers to words
+	};
+	static const int cnam[16] = {
+		6, 7, 7, 7, 8, 9,10,11,12,12,12,12,13,13,13,13 //refers to words
+	};
+	static const QString oper[16] = {
+		"IMP",  "AND",  "OR",  "EOR",  "NOT",  "---", "---", "---",
+		"TIMP", "TAND", "TOR", "TEOR", "TNOT", "---", "---", "---",
+	};
+	static const int argbold[16] = {
+		0x00, 0x00, 0x00, 0x00, 0x10, 0x20, 0x26, 0x2D,
+		0x2C, 0x2C, 0x1C, 0x2C, 0x2C, 0x3C, 0x2C, 0x2C,
+	};
+	static const int regbold[16] = {
+		0x00, 0x00, 0x00, 0x00, 0x83, 0xCC, 0xC3, 0xFC,
+		0xFC, 0xBF, 0xB3, 0xFC, 0xFC, 0xBF, 0xAE, 0xFC,
+	};
 
-	QString explication=QString(
-	"Command: <b>%1</b> Destination: <b>%2</b> Source: <b>%3</b><br>Data: <b>%4</b> Operation: <b>%5</b>"
-	).
-	arg(words[cnam[cmd]]).
-	arg(words[dest[cmd]]).
-	arg(words[srce[cmd]]).
-	arg(words[data[cmd]]).
-	arg(oper[operat]);
+	int operat = (val >> 0) & 15;
+	int cmd    = (val >> 4) & 15;
 
+	QString explication = QString(
+		"Command: <b>%1</b> "
+		"Destination: <b>%2</b> "
+		"Source: <b>%3</b><br>"
+		"Data: <b>%4</b> "
+		"Operation: <b>%5</b>").
+			arg(words[cnam[cmd]]).
+			arg(words[dest[cmd]]).
+			arg(words[srce[cmd]]).
+			arg(words[data[cmd]]).
+			arg(oper[operat]);
 	label_expl1->setText(explication);
 
 	//a Normal and a Bold font
@@ -166,24 +175,21 @@ void VDPCommandRegViewer::decodeR46( int val )
 	QFont fontB;
 	fontB.setBold(true);
 	//set the checkbox font in bold if command uses the corresponding bit
-        QCheckBox* item;
-	for (int b=0 ; b<7 ; b++){
-		item = this->findChild<QCheckBox*>(QString("checkBoxBit_%1").arg(b));
-		item->setFont( (argbold[cmd] & (1<<b)) ? fontB : fontN );
-	};
-	labelSX->setFont( (regbold[cmd] & 1) ? fontB : fontN );
-	labelSY->setFont( (regbold[cmd] & 2) ? fontB : fontN );
-	labelDX->setFont( (regbold[cmd] & 4) ? fontB : fontN );
-	labelDY->setFont( (regbold[cmd] & 8) ? fontB : fontN );
-	labelNX->setFont( (regbold[cmd] & 16) ? fontB : fontN );
-	labelNY->setFont( (regbold[cmd] & 32) ? fontB : fontN );
-	labelCOLOR->setFont( (regbold[cmd] & 64) ? fontB : fontN );
-	labelARG->setFont( (regbold[cmd] & 128) ? fontB : fontN );
-
+	for (int b = 0; b < 7; ++b) {
+		QCheckBox* item = findChild<QCheckBox*>(QString("checkBoxBit_%1").arg(b));
+		item->setFont((argbold[cmd] & (1 << b)) ? fontB : fontN);
+	}
+	labelSX   ->setFont((regbold[cmd] & 0x01) ? fontB : fontN);
+	labelSY   ->setFont((regbold[cmd] & 0x02) ? fontB : fontN);
+	labelDX   ->setFont((regbold[cmd] & 0x04) ? fontB : fontN);
+	labelDY   ->setFont((regbold[cmd] & 0x08) ? fontB : fontN);
+	labelNX   ->setFont((regbold[cmd] & 0x10) ? fontB : fontN);
+	labelNY   ->setFont((regbold[cmd] & 0x20) ? fontB : fontN);
+	labelCOLOR->setFont((regbold[cmd] & 0x40) ? fontB : fontN);
+	labelARG  ->setFont((regbold[cmd] & 0x80) ? fontB : fontN);
 
 	//TODO: create basic equivalent
 	//TODO: warn about wrong stuff (majorant < minorant when line drawing for instance)
-
 }
 
 void VDPCommandRegViewer::on_syncPushButton_clicked()
@@ -194,22 +200,22 @@ void VDPCommandRegViewer::on_syncPushButton_clicked()
 void VDPCommandRegViewer::on_launchPushButton_clicked()
 {
 	unsigned char newregs[64];
-	memset(newregs,0,64);
-	newregs[32]=lineEdit_r32->text().toInt(NULL,0);
-	newregs[33]=lineEdit_r33->text().toInt(NULL,0);
-	newregs[34]=lineEdit_r34->text().toInt(NULL,0);
-	newregs[35]=lineEdit_r35->text().toInt(NULL,0);
-	newregs[36]=lineEdit_r36->text().toInt(NULL,0);
-	newregs[37]=lineEdit_r37->text().toInt(NULL,0);
-	newregs[38]=lineEdit_r38->text().toInt(NULL,0);
-	newregs[39]=lineEdit_r39->text().toInt(NULL,0);
-	newregs[40]=lineEdit_r40->text().toInt(NULL,0);
-	newregs[41]=lineEdit_r41->text().toInt(NULL,0);
-	newregs[42]=lineEdit_r42->text().toInt(NULL,0);
-	newregs[43]=lineEdit_r43->text().toInt(NULL,0);
-	newregs[44]=lineEdit_r44->text().toInt(NULL,0);
-	newregs[45]=lineEdit_r45->text().toInt(NULL,0);
-	newregs[46]=lineEdit_r46->text().toInt(NULL,0);
+	memset(newregs, 0, 64);
+	newregs[32] = lineEdit_r32->text().toInt(NULL, 0);
+	newregs[33] = lineEdit_r33->text().toInt(NULL, 0);
+	newregs[34] = lineEdit_r34->text().toInt(NULL, 0);
+	newregs[35] = lineEdit_r35->text().toInt(NULL, 0);
+	newregs[36] = lineEdit_r36->text().toInt(NULL, 0);
+	newregs[37] = lineEdit_r37->text().toInt(NULL, 0);
+	newregs[38] = lineEdit_r38->text().toInt(NULL, 0);
+	newregs[39] = lineEdit_r39->text().toInt(NULL, 0);
+	newregs[40] = lineEdit_r40->text().toInt(NULL, 0);
+	newregs[41] = lineEdit_r41->text().toInt(NULL, 0);
+	newregs[42] = lineEdit_r42->text().toInt(NULL, 0);
+	newregs[43] = lineEdit_r43->text().toInt(NULL, 0);
+	newregs[44] = lineEdit_r44->text().toInt(NULL, 0);
+	newregs[45] = lineEdit_r45->text().toInt(NULL, 0);
+	newregs[46] = lineEdit_r46->text().toInt(NULL, 0);
 
 	WriteDebugBlockCommand* req = new WriteDebugBlockCommand(
 		"{VDP regs}", 32, 15, newregs);
@@ -218,36 +224,36 @@ void VDPCommandRegViewer::on_launchPushButton_clicked()
 
 void VDPCommandRegViewer::on_lineEdit_r44_editingFinished()
 {
-	int val=lineEdit_r44->text().toInt(NULL,0);
-	label_color->setText(QString("%1 %2").arg((val>>4)&15,4,2,QChar('0')).arg(val&15,4,2,QChar('0')));
+	int val = lineEdit_r44->text().toInt(NULL, 0);
+	label_color->setText(QString("%1 %2").
+			arg((val >> 4) & 15, 4, 2, QChar('0')).
+			arg((val >> 0) & 15, 4, 2, QChar('0')));
 }
 
 void VDPCommandRegViewer::R45BitChanged(int state)
 {
 	//for now simply recheck all the checkBoxes and recreate R45
-	QList<QCheckBox*> list;
-        list = this->findChildren<QCheckBox*>(); // Warning:  MSVC 6!! 
-        QCheckBox* item;
 	int r45 = 0;
-        foreach (item , list){
-		if (item->isChecked()){
-	                int order= QString(item->objectName().right(1)).toInt();
-			r45 = r45 | (1<<order);
+	QList<QCheckBox*> list = findChildren<QCheckBox*>();
+	QCheckBox* item;
+	foreach(item, list) {
+		if (item->isChecked()) {
+			int order = QString(item->objectName().right(1)).toInt();
+			r45 = r45 | (1 << order);
 		}
-        };
-	lineEdit_r45->setText(QString("0x%1").arg(r45,2,16,QChar('0')).toUpper());
-	label_arg->setText(QString("%1").arg(r45,8,2,QChar('0')));
+	}
+	lineEdit_r45->setText(QString("0x%1").arg(r45, 2, 16, QChar('0')).toUpper());
+	label_arg->setText(QString("%1").arg(r45, 8, 2, QChar('0')));
 }
 
 void VDPCommandRegViewer::on_lineEdit_r46_editingFinished()
 {
-	bool ok;
-	int val = 255 & lineEdit_r46->text().toInt(&ok,0);
-	lineEdit_r46->setText(QString("0x%1").arg(val,2,16,QChar('0')).toUpper());
-	R46=val;
+	int val = lineEdit_r46->text().toInt(NULL, 0) & 0xFF;
+	lineEdit_r46->setText(QString("0x%1").arg(val, 2, 16 ,QChar('0')).toUpper());
+	R46 = val;
 	decodeR46(R46);
-	comboBox_operator->setCurrentIndex(15 & R46);
-	comboBox_cmd->setCurrentIndex(15 & (R46>>4)); // this might hide the operator again :-)
+	comboBox_operator->setCurrentIndex(R46 & 15);
+	comboBox_cmd->setCurrentIndex((R46 >> 4) & 15); // this might hide the operator again :-)
 }
 
 void VDPCommandRegViewer::refresh()
@@ -258,8 +264,8 @@ void VDPCommandRegViewer::refresh()
 	new SimpleHexRequest(
 		"debug_bin2hex "
 		"[ debug read_block {VDP regs} 0 64 ]"
-		"[ debug read_block {VDP status regs} 0 16 ]"
-		,64+16,regs, *this);
+		"[ debug read_block {VDP status regs} 0 16 ]",
+		64 + 16, regs, *this);
 }
 
 
@@ -308,10 +314,10 @@ void VDPCommandRegViewer::DataHexRequestReceived()
 
 const QString VDPCommandRegViewer::hex8bit(int val)
 {
-	return QString("0x") + QString("%1").arg(val,2,16,QChar('0')).toUpper();
+	return QString("0x%1").arg(val, 2, 16, QChar('0')).toUpper();
 }
 
 const QString VDPCommandRegViewer::hex16bit(int val)
 {
-	return QString("0x") + QString("%1").arg(val,4,16,QChar('0')).toUpper();
+	return QString("0x%1").arg(val, 4, 16, QChar('0')).toUpper();
 }
--- src/VDPCommandRegViewer.h
+++ src/VDPCommandRegViewer.h
@@ -1,13 +1,9 @@
 #ifndef VDPCOMMANDREGVIEWER_H
 #define VDPCOMMANDREGVIEWER_H
 
-#include <QDialog>
-#include "OpenMSXConnection.h"
-#include "CommClient.h"
-#include "Settings.h"
 #include "SimpleHexRequest.h"
-
 #include "ui_VDPCommandRegisters.h"
+#include <QDialog>
 
 
 class view88to16 : public QObject
@@ -16,64 +12,63 @@ class view88to16 : public QObject
 public:
 	view88to16(QWidget* a, QWidget* b, QWidget* c)
 	{
-		rl=rh=rw=-1;
-		disp_rl=disp_rh=1;
-		disp_rw=2;
+		rl = rh = rw = -1;
+		disp_rl = disp_rh = 1;
+		disp_rw = 2;
 		w_rl = a;
 		w_rh = b;
 		w_rw = c;
-	};
+	}
 	
-	int getRL(){ return rl; };
-	int getRH(){ return rh; };
-	int getRW(){ return rw; };
+	int getRL() const { return rl; }
+	int getRH() const { return rh; }
+	int getRW() const { return rw; }
 
-	void setDispRL(int mode){ disp_rl = mode; };
-	void setDispRH(int mode){ disp_rh = mode; };
-	void setDispRW(int mode){ disp_rw = mode; };
+	void setDispRL(int mode) { disp_rl = mode; }
+	void setDispRH(int mode) { disp_rh = mode; }
+	void setDispRW(int mode) { disp_rw = mode; }
 
-	void setWidgetRL(QWidget* wdgt){ w_rl = wdgt; };
-	void setWidgetRH(QWidget* wdgt){ w_rh = wdgt; };
-	void setWidgetRW(QWidget* wdgt){ w_rw = wdgt; };
+	void setWidgetRL(QWidget* wdgt) { w_rl = wdgt; }
+	void setWidgetRH(QWidget* wdgt) { w_rh = wdgt; }
+	void setWidgetRW(QWidget* wdgt) { w_rw = wdgt; }
 
 public slots:
+	void finishRH() { setRH(getWidgetText(w_rh)); }
+	void finishRL() { setRL(getWidgetText(w_rl)); }
+	void finishRW() { setRW(getWidgetText(w_rw)); }
 
-	void finishRH(){ setRH(getWidgetText(w_rh)); };
-	void finishRL(){ setRL(getWidgetText(w_rl)); };
-	void finishRW(){ setRW(getWidgetText(w_rw)); };
-
-	void setRH(const QString newval)
+	void setRH(const QString& newval)
 	{
 		bool ok;
-		int val = 255 & newval.toInt(&ok,0);
-		if (!ok ) return;
-		if (val==rh) return;
+		int val = newval.toInt(&ok, 0) & 0xFF;
+		if (!ok || (val == rh)) return;
+
 		rh = val;
 		updaterw();
 		updaterh();
-	};
-	void setRL(const QString newval)
+	}
+	void setRL(const QString& newval)
 	{
 		bool ok;
-		int val = 255 & newval.toInt(&ok,0);
-		if (!ok ) return;
-		if (val==rl) return;
+		int val = newval.toInt(&ok, 0) & 0xFF;
+		if (!ok || (val == rl)) return;
+
 		rl = val;
 		updaterw();
 		updaterl();
-	};
-	void setRW(const QString newval)
+	}
+	void setRW(const QString& newval)
 	{
 		//TODO: build a split-in-two method
 		bool ok;
-		int val = 65535 & newval.toInt(&ok,0);
-		if (!ok ) return;
-		if (val==rw) return;
+		int val = newval.toInt(&ok, 0) & 0xFFFF;
+		if (!ok || (val == rw)) return;
+
 		rw = val;
 		updaterl();
 		updaterh();
 		updaterw();
-	};
+	}
 
 private:
 	int rh;
@@ -86,10 +81,10 @@ private:
 	QWidget* w_rl;
 	QWidget* w_rw;
 
-	QString convert(int val,int mode)
+	QString convert(int val, int mode)
 	{
-		if (mode & 1){
-			return QString("0x%1").arg(val,2,16,QChar('0'));
+		if (mode & 1) {
+			return QString("0x%1").arg(val, 2, 16, QChar('0'));
 		} else {
 			return QString("%1").arg(val);
 		}
@@ -98,59 +93,60 @@ private:
 	QString getWidgetText(QWidget* wdg)
 	{
 		if (wdg == NULL) return QString();
-		QLabel* ql=dynamic_cast<QLabel*>( wdg );
-		if (ql != NULL){
+		if (QLabel* ql = dynamic_cast<QLabel*>(wdg)) {
 			return ql->text();
 		} else {
-			QLineEdit* qe=dynamic_cast<QLineEdit*>( wdg );
+			QLineEdit* qe = dynamic_cast<QLineEdit*>(wdg);
 			return qe->text();
 		}
-	};
+	}
 
 	void updateWidget(QWidget* wdg, int val, int mode)
 	{
-		QLabel* ql=dynamic_cast<QLabel*>( wdg );
-		if (ql != NULL){
-			ql->setText( convert(val,mode) );
+		if (QLabel* ql = dynamic_cast<QLabel*>(wdg)) {
+			ql->setText(convert(val, mode));
 		} else {
-			QLineEdit* qe=dynamic_cast<QLineEdit*>( wdg );
-			qe->setText( convert(val,mode) );
+			QLineEdit* qe = dynamic_cast<QLineEdit*>(wdg);
+			qe->setText(convert(val, mode));
 		}
-	};
+	}
 	
- 	void updaterw()
+	void updaterw()
 	{
 		if (w_rw == NULL) return;
-		rw=rl+256*rh;
-		updateWidget(w_rw,rw,disp_rw);
-
-	};
-
- 	void updaterl()
+		rw = rl + 256 * rh;
+		updateWidget(w_rw, rw, disp_rw);
+	}
+	void updaterl()
 	{
 		if (w_rl == NULL) return;
-		rl=rw & 255;
-		updateWidget(w_rl,rl,disp_rl);
-
-	};
-
- 	void updaterh()
+		rl = rw & 255;
+		updateWidget(w_rl, rl, disp_rl);
+	}
+	void updaterh()
 	{
 		if (w_rh == NULL) return;
-		rh=(rw>>8) & 255;
-		updateWidget(w_rh,rh,disp_rh);
-	};
-
+		rh = (rw >> 8) & 255;
+		updateWidget(w_rh, rh, disp_rh);
+	}
 };
 
-class VDPCommandRegViewer : public QDialog, public SimpleHexRequestUser, private Ui::VDPCmdRegs
+
+class VDPCommandRegViewer : public QDialog, public SimpleHexRequestUser,
+	                    private Ui::VDPCmdRegs
 {
 	Q_OBJECT
 public:
-	VDPCommandRegViewer( QWidget *parent = 0);
+	VDPCommandRegViewer(QWidget* parent = 0);
 	~VDPCommandRegViewer();
 
 private:
+	virtual void DataHexRequestReceived();
+	void decodeR46(int val);
+	const QString hex8bit(int val);
+	const QString hex16bit(int val);
+	void syncRegToCmd();
+
 	unsigned char* regs;
 	unsigned char* statusregs;
 	view88to16* grp_l_sx;
@@ -165,15 +161,7 @@ private:
 	view88to16* grp_dy;
 	view88to16* grp_nx;
 	view88to16* grp_ny;
-
 	int R46;
-
-protected:
-	virtual void DataHexRequestReceived();
-	void decodeR46(int val);
-	const QString hex8bit(int val);
-	const QString hex16bit(int val);
-	void syncRegToCmd();
 
 public slots:
 	void refresh();
@@ -181,10 +169,10 @@ public slots:
 	void on_lineEdit_r44_editingFinished();
 	void on_lineEdit_r45_editingFinished();
 	void on_lineEdit_r46_editingFinished();
-	void on_comboBox_cmd_currentIndexChanged ( int index ) ;
-	void on_comboBox_operator_currentIndexChanged ( int index ) ;
-	void on_syncPushButton_clicked() ;
-	void on_launchPushButton_clicked() ;
+	void on_comboBox_cmd_currentIndexChanged(int index);
+	void on_comboBox_operator_currentIndexChanged(int index);
+	void on_syncPushButton_clicked();
+	void on_launchPushButton_clicked();
 };
 
 #endif /* VDPCOMMANDREGVIEWER_H */
