1b814d29b5a990368ad052f8d38ad0634bdb6bfe
Wouter Vermaelen <m9710797@users.sourceforge.net>
2009-03-03 17:54:18 +0000

    cleanups WIP, BitMapViewer
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@9324 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/BitMapViewer.cpp
+++ src/BitMapViewer.cpp
@@ -1,7 +1,7 @@
 #include "BitMapViewer.h"
 #include "VramBitMappedView.h"
-#include <QMessageBox>
 #include "VDPDataStore.h"
+#include <QMessageBox>
 
 static const unsigned char defaultPalette[32] = {
 //        RB  G
@@ -23,15 +23,16 @@ static const unsigned char defaultPalette[32] = {
 	0x77, 7,
 };
 
-BitMapViewer::BitMapViewer( QWidget *parent)
+BitMapViewer::BitMapViewer(QWidget* parent)
 	: QDialog(parent)
 {
 	setupUi(this);
-	// hand code entering the actual display widget in the scrollarea
-	// With the designer-qt4 there is an extra scrollAreaWidget between 
-	// the imageWidget and the QScrollArea so the scrollbars are not 
-	// correctly handled when the image is resized. (since the intermediate widget stays the same size)
-	// I did not try to have this intermediate widget resize and all, since it was superflous anyway.
+	// hand code entering the actual display widget in the scrollarea With
+	// the designer-qt4 there is an extra scrollAreaWidget between the
+	// imageWidget and the QScrollArea so the scrollbars are not correctly
+	// handled when the image is resized. (since the intermediate widget
+	// stays the same size). I did not try to have this intermediate widget
+	// resize and all, since it was superflous anyway.
 	imageWidget = new VramBitMappedView();
 	QSizePolicy sizePolicy1(QSizePolicy::Fixed, QSizePolicy::Fixed);
 	sizePolicy1.setHorizontalStretch(0);
@@ -42,74 +43,77 @@ BitMapViewer::BitMapViewer( QWidget *parent)
 
 	scrollArea->setWidget(imageWidget);
 
-	lines=212;
-	vramSize=lines*256;
-	useVDP=true;
+	useVDP = true;
+
 	const unsigned char* vram    = VDPDataStore::instance().getVramPointer();
 	const unsigned char* palette = VDPDataStore::instance().getPalettePointer();
-	vramAddress=0;
 	imageWidget->setVramSource(vram);
 	imageWidget->setVramAddress(0);
 	imageWidget->setPaletteSource(palette);
 
 	//now hook up some signals and slots
-	connect( &VDPDataStore::instance(), SIGNAL( dataRefreshed() ), this, SLOT( VDPDataStoreDataRefreshed() ) );
-	connect( &VDPDataStore::instance(), SIGNAL( dataRefreshed() ), imageWidget, SLOT( refresh() ) );
-	connect( refreshButton,  SIGNAL( clicked (bool) ), &VDPDataStore::instance(), SLOT( refresh() ) );
+	connect(&VDPDataStore::instance(), SIGNAL(dataRefreshed()),
+	        this, SLOT(VDPDataStoreDataRefreshed()));
+	connect(&VDPDataStore::instance(), SIGNAL(dataRefreshed()),
+	        imageWidget, SLOT(refresh()));
+	connect(refreshButton, SIGNAL(clicked(bool)),
+	        &VDPDataStore::instance(), SLOT(refresh()));
 
-	connect( imageWidget,  SIGNAL( imagePosition (int,int,int,unsigned int,int) ), this, SLOT( imagePositionUpdate(int,int,int,unsigned int,int) ) );
-	connect( imageWidget,  SIGNAL( imageClicked (int,int,int,unsigned int,int) ), this, SLOT( imagePositionUpdate(int,int,int,unsigned int,int) ) );
+	connect(imageWidget, SIGNAL(imagePosition(int,int,int,unsigned int,int)),
+	        this, SLOT(imagePositionUpdate(int,int,int,unsigned int,int)));
+	connect(imageWidget, SIGNAL(imageClicked (int,int,int,unsigned int,int)),
+	        this, SLOT(imagePositionUpdate(int,int,int,unsigned int,int)));
 
 	// and now go fetch the initial data
 	VDPDataStore::instance().refresh();
-}
-
-BitMapViewer::~BitMapViewer()
-{
 }
 
 void BitMapViewer::decodeVDPregs()
 {
 	const unsigned char* regs = VDPDataStore::instance().getRegsPointer();
 
-	//Get the number of lines from bit x from reg Y
-	int v = (regs[9]&128)?212:192;
-	printf("\nlines acording to the bits %i,: %i\n",(regs[9]&128),v);
-	linesLabel->setText(QString("%1").arg(v,0,10));
-	if (useVDP) linesVisible->setCurrentIndex( (regs[9]&128)?1:0 );
+	// Get the number of lines
+	int v1 = (regs[9] & 128) ? 212 : 192;
+	printf("\nlines acording to the bits %i,: %i\n", (regs[9] & 128), v1);
+	linesLabel->setText(QString("%1").arg(v1, 0, 10));
+	if (useVDP) linesVisible->setCurrentIndex((regs[9] & 128) ? 1 : 0);
 	
-	// Get the border color from register 7
-	v = 15 & regs[7];
-	printf("\nborder acording to the regs %i,: %i\n",(regs[7]),v);
-	if (regs[8]&32) v=0;
-	printf("\ncolor 0 is pallet regs %i,: %i\n",(regs[8]&32),v);
-	borderLabel->setText(QString("%1").arg(v,0,10));
-	if (useVDP) bgColor->setValue(v);
+	// Get the border color
+	int v2 = regs[7] & 15;
+	printf("\nborder acording to the regs %i,: %i\n", regs[7], v2);
+	if (regs[8] & 32) v2 = 0;
+	printf("\ncolor 0 is pallet regs %i,: %i\n", (regs[8] & 32), v2);
+	borderLabel->setText(QString("%1").arg(v2, 0, 10));
+	if (useVDP) bgColor->setValue(v2);
 
-	//Get current screenmode
-	v=((regs[0]&14)<<1) |((regs[1]&24)>>3);
-	printf("screenMod according to the bits: %i\n",v);
-	int bits_modetxt[]={   1,  3,  0,255,  2,255,255,255,
-			       4,255, 80,255,  5,255,255,255,
-			       6,255,255,255,  7,255,255,255,
-			     255,255,255,255,  8,255,255,255
-			};
-	modeLabel->setText(QString("%1").arg(bits_modetxt[v],0,10));
-	int bits_mode[]={ 0,0,0,0,0,0,0,0,
-			  0,0,0,0,0,0,0,0,
-			  1,0,0,0,0,0,0,0,
-			  0,0,0,0,3,0,0,0
-			};
-	if (useVDP) screenMode->setCurrentIndex(bits_mode[v]);
+	// Get current screenmode
+	static const int bits_modetxt[32] = {
+		  1,   3,   0, 255,  2, 255, 255, 255,
+		  4, 255,  80, 255,  5, 255, 255, 255,
+		  6, 255, 255, 255,  7, 255, 255, 255,
+		255, 255, 255, 255,  8, 255, 255, 255,
+	};
+	static const int bits_mode[32] = {
+		0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0,
+		1, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 3, 0, 0, 0,
+	};
+	int v3 = ((regs[0] & 0x0E) << 1) | ((regs[1] & 0x18) >> 3);
+	printf("screenMod according to the bits: %i\n", v3);
+	modeLabel->setText(QString("%1").arg(bits_modetxt[v3], 0, 10));
+	if (useVDP) screenMode->setCurrentIndex(bits_mode[v3]);
 
-	//get the current visible page
-	unsigned p = (regs[2]>>5)&3;
-	unsigned q = 1<<15;
-	if (bits_modetxt[v]>6)  { p&=1; q<<=1 ;};
+	// Get the current visible page
+	unsigned p = (regs[2] >> 5) & 3;
+	unsigned q = 1 << 15;
+	if (bits_modetxt[v3] > 6) {
+		p &= 1;
+		q <<= 1;
+	}
 	printf("visible page according to the bits: %i\n",p);
-	addressLabel->setText(QString("0x%1").arg(p*q,5,16,QChar('0')));
+	addressLabel->setText(QString("0x%1").arg(p * q, 5, 16, QChar('0')));
 	if (useVDP) showPage->setCurrentIndex(p);
-
 }
 
 
@@ -119,14 +123,12 @@ void BitMapViewer::refresh()
 	VDPDataStore::instance().refresh();
 }
 
-void BitMapViewer::on_screenMode_currentIndexChanged( const QString & text  )
+void BitMapViewer::on_screenMode_currentIndexChanged(const QString& text)
 {
-	screenMod=text.toInt();
-	printf("\nnew screenMod: %i\n",screenMod);
+	screenMod = text.toInt();
+	printf("\nnew screenMod: %i\n", screenMod);
 	//change then number of visibe pages when this changes
-	vramAddress=0;
-	vramSize=lines*256;
-	imageWidget->setVramAddress(vramAddress);
+	imageWidget->setVramAddress(0);
 	// make sure that we are decoding new mode from page 0, imagine viewing
 	// page 3 of screen5 and then switching to screen 8 without changing the
 	// starting vram address....
@@ -134,43 +136,39 @@ void BitMapViewer::on_screenMode_currentIndexChanged( const QString & text  )
 	showPage->clear();
 	showPage->insertItem(0, "0");
 	showPage->insertItem(1, "1");
-	if (screenMod < 7){
-		vramSize=lines*128;
+	if (screenMod < 7) {
 		showPage->insertItem(2, "2");
 		showPage->insertItem(3, "3");
 	}
 }
 
-void BitMapViewer::on_showPage_currentIndexChanged( int index )
+void BitMapViewer::on_showPage_currentIndexChanged(int index)
 {
-	//if this is the consequence of a a .clear() in the on_screenMode_currentIndexChanged
-	// then we do nothing!
+	//if this is the consequence of a .clear() in the
+	// on_screenMode_currentIndexChanged we do nothing!
 	if (index == -1) return;
-	showpage=index;
 
-	int m1[] = {0x00000 , 0x08000, 0x10000, 0x18000 };
-	printf("\nvoid BitMapViewer::on_showPage_currentIndexChanged( int %i);\n",index);
-	if (screenMod > 7 ) index *= 2;
-	vramAddress = m1[index];
-	printf("vramAddress %i\n",vramAddress);
+	static const int m1[4] = { 0x00000 , 0x08000, 0x10000, 0x18000 };
+	printf("\nvoid BitMapViewer::on_showPage_currentIndexChanged( int %i);\n", index);
+	if (screenMod > 7) index *= 2;
+	int vramAddress = m1[index];
+	printf("vramAddress %i\n", vramAddress);
 	imageWidget->setVramAddress(vramAddress);
-
 }
 
-void BitMapViewer::on_linesVisible_currentIndexChanged( int index )
+void BitMapViewer::on_linesVisible_currentIndexChanged(int index)
 {
-	int m[] = {192,212,256};
-	lines = m[index];
-	vramSize=lines*((screenMod < 7)?128:256);
+	static const int m[3] = { 192, 212, 256 };
+	int lines = m[index];
 	imageWidget->setLines(lines);
 }
 
-void BitMapViewer::on_bgColor_valueChanged ( int value )
+void BitMapViewer::on_bgColor_valueChanged(int value)
 {
 	imageWidget->setBorderColor(value);
 }
 
-void BitMapViewer::on_useVDPRegisters_stateChanged ( int state )
+void BitMapViewer::on_useVDPRegisters_stateChanged(int state)
 {
 	useVDP = state;
 
@@ -182,20 +180,27 @@ void BitMapViewer::on_useVDPRegisters_stateChanged ( int state )
 	imageWidget->refresh();
 }
 
-void BitMapViewer::on_zoomLevel_valueChanged ( double d )
+void BitMapViewer::on_zoomLevel_valueChanged(double d)
 {
 	imageWidget->setZoom(float(d));
 }
 
-void BitMapViewer::on_saveImageButton_clicked( bool checked )
+void BitMapViewer::on_saveImageButton_clicked(bool checked)
 {
-	QMessageBox::information(this,"Not yet implemented","Sorry, the save image dialog is not yet implemented");
+	QMessageBox::information(
+		this,
+		"Not yet implemented",
+		"Sorry, the save image dialog is not yet implemented");
 }
 
-void BitMapViewer::on_editPaletteButton_clicked( bool checked )
+void BitMapViewer::on_editPaletteButton_clicked(bool checked)
 {
 	useVDPPalette->setChecked(false);
-	QMessageBox::information(this,"Not yet implemented","Sorry, the palette editor is not yet implementedi,only disabling 'Use VDP palette registers' for now");
+	QMessageBox::information(
+		this,
+		"Not yet implemented",
+		"Sorry, the palette editor is not yet implemented, "
+		"only disabling 'Use VDP palette registers' for now");
 }
 
 void BitMapViewer::on_useVDPPalette_stateChanged(int state)
@@ -206,11 +211,10 @@ void BitMapViewer::on_useVDPPalette_stateChanged(int state)
 	} else {
 		imageWidget->setPaletteSource(defaultPalette);
 	}
-	//refresh();
 	imageWidget->refresh();
 }
 /*
-void BitMapViewer::on_refreshButton_clicked( bool checked )
+void BitMapViewer::on_refreshButton_clicked(bool checked)
 {
 	refresh();
 	decodeVDPregs();
@@ -223,12 +227,12 @@ void BitMapViewer::VDPDataStoreDataRefreshed()
 	decodeVDPregs();
 }
 
-void BitMapViewer::imagePositionUpdate(int x,int y,int color, unsigned addr, int byteValue)
+void BitMapViewer::imagePositionUpdate(
+	int x, int y, int color, unsigned addr, int byteValue)
 {
-	labelX->setText(QString("%1").arg(x,3,10,QChar('0')));
-	labelY->setText(QString("%1").arg(y,3,10,QChar('0')));
-	labelColor->setText(QString("%1").arg(color,3,10,QChar('0')));
-	labelByte->setText(QString("0x%1").arg(byteValue,2,16,QChar('0')));
-	labelVramAddr->setText(QString("0x%1").arg(addr,5,16,QChar('0')));
-
+	labelX->setText(QString("%1").arg(x, 3, 10, QChar('0')));
+	labelY->setText(QString("%1").arg(y, 3, 10, QChar('0')));
+	labelColor->setText(QString("%1").arg(color, 3, 10, QChar('0')));
+	labelByte->setText(QString("0x%1").arg(byteValue, 2, 16, QChar('0')));
+	labelVramAddr->setText(QString("0x%1").arg(addr, 5, 16, QChar('0')));
 }
--- src/BitMapViewer.h
+++ src/BitMapViewer.h
@@ -1,51 +1,40 @@
 #ifndef BITMAPVIEWER_OPENMSX_H
 #define BITMAPVIEWER_OPENMSX_H
 
-#include <QDialog>
-#include <QColor>
-#include "OpenMSXConnection.h"
-#include "CommClient.h"
-#include "Settings.h"
-#include "VramBitMappedView.h"
 #include "ui_BitMapViewer.h"
+#include <QDialog>
+
+class VramBitMappedView;
 
 class BitMapViewer : public QDialog, private Ui::BitMapViewer
 {
 	Q_OBJECT
 public:
-	BitMapViewer( QWidget *parent = 0);
-	~BitMapViewer();
+	BitMapViewer(QWidget* parent = 0);
 
 private:
-	int screenMod;
-	int lines;
-	int showpage;
-	int borderColor;
-	bool useVDP;
-
-	int vramAddress;
-	int vramSize;
-
 	void decodeVDPregs();
 
-	VramBitMappedView *imageWidget;
+	VramBitMappedView* imageWidget;
+	int screenMod;
+	bool useVDP;
 
 private slots:
 	void refresh();
 
-	void on_screenMode_currentIndexChanged( const QString & text );
-	void on_showPage_currentIndexChanged( int index );
-	void on_linesVisible_currentIndexChanged( int index );
-	void on_bgColor_valueChanged ( int value );
+	void on_screenMode_currentIndexChanged(const QString& text);
+	void on_showPage_currentIndexChanged(int index);
+	void on_linesVisible_currentIndexChanged(int index);
+	void on_bgColor_valueChanged(int value);
 
-	void on_useVDPRegisters_stateChanged ( int state );
+	void on_useVDPRegisters_stateChanged(int state);
 
-	void on_saveImageButton_clicked ( bool checked );
-	void on_editPaletteButton_clicked ( bool checked );
-	void on_useVDPPalette_stateChanged ( int state );
-	void on_zoomLevel_valueChanged ( double d );
+	void on_saveImageButton_clicked(bool checked);
+	void on_editPaletteButton_clicked(bool checked);
+	void on_useVDPPalette_stateChanged(int state);
+	void on_zoomLevel_valueChanged(double d);
 
-	void imagePositionUpdate(int x, int y, int color, unsigned addr,int byteValue);
+	void imagePositionUpdate(int x, int y, int color, unsigned addr, int byteValue);
 
 	void VDPDataStoreDataRefreshed();
 };
