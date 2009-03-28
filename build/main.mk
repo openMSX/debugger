8ec5ca1bc0205e0ba54489147225ae7ede7e507f
Maarten ter Huurne <maarten@treewalker.org>
2009-03-28 17:42:36 +0000

    Put all source dirs in the include path, not just "src".
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@9434 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- build/main.mk
+++ build/main.mk
@@ -243,7 +243,8 @@ endif
 
 CXX:=g++
 CXXFLAGS:= -g
-COMPILE_FLAGS:=$(addprefix -I,$(QT_HEADER_DIRS) $(SOURCES_PATH) $(GEN_SRC_PATH))
+INCLUDE_INTERNAL:=$(sort $(foreach header,$(HEADERS_FULL),$(patsubst %/,%,$(dir $(header)))))
+COMPILE_FLAGS:=$(addprefix -I,$(QT_HEADER_DIRS) $(INCLUDE_INTERNAL) $(GEN_SRC_PATH))
 ifeq ($(OPENMSX_TARGET_OS),darwin)
 LINK_FLAGS:=-F$(QT_INSTALL_LIBS) $(addprefix -framework Qt,$(QT_COMPONENTS))
 SDK_PATH:=/Developer/SDKs/MacOSX10.4u.sdk
