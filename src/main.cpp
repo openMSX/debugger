278a32da93b6fd7915b3661702b90b84160baca9
Wouter Vermaelen <m9710797@users.sourceforge.net>
2010-01-13 18:25:57 +0000

    Fixed compilation error with gcc-4.5
    git-svn-id: file:///openmsx-svn/code/openmsx-debugger/trunk@11151 7512d703-071c-0410-b5c2-d2bdd895b7d1

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv)
 // Don't set the icon on OS X, because it will replace the high-res version
 // with a lower resolution one, even though openMSX-debugger-logo-128.png is 128x128.
 #ifndef __APPLE__
-	app.setWindowIcon(QIcon::QIcon(":icons/openMSX-debugger-logo-128.png"));
+	app.setWindowIcon(QIcon(":icons/openMSX-debugger-logo-128.png"));
 #endif
 	// restore main settings
 	app.setFont(Settings::get().font(Settings::APP_FONT));
