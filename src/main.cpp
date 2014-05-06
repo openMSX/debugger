a07a65e31056ed0b8a30918b9ae34ede08070e3a
Manuel Bilderbeek <Manuel.Bilderbeek@gmail.com>
2014-05-06 23:27:36 +0200

    Add 256x256 icon and use it.

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char** argv)
 {
 	QApplication app(argc, argv);
 // Don't set the icon on OS X, because it will replace the high-res version
-// with a lower resolution one, even though openMSX-debugger-logo-128.png is 128x128.
+// with a lower resolution one, even though openMSX-debugger-logo-256.png is 256x256.
 #ifndef __APPLE__
-	app.setWindowIcon(QIcon(":icons/openMSX-debugger-logo-128.png"));
+	app.setWindowIcon(QIcon(":icons/openMSX-debugger-logo-256.png"));
 #endif
 	// restore main settings
 	app.setFont(Settings::get().font(Settings::APP_FONT));
