bf1a2c74aa41f05f7d0b7617aa87c851f4cc933f
Pedro de Medeiros <pedro.medeiros@gmail.com>
2022-02-18 09:55:43 -0300

    deprecation warnings

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -281,7 +281,7 @@ bool SymbolTable::readSymbolFile(
 	QTextStream in(&file);
 	while (!in.atEnd()) {
 		QString line = in.readLine();
-		QStringList l = line.split(equ, QString::SplitBehavior::KeepEmptyParts, Qt::CaseInsensitive);
+		QStringList l = line.split(equ, Qt::SplitBehaviorFlags::KeepEmptyParts, Qt::CaseInsensitive);
 		if (l.size() != 2) continue;
 		int value;
 		if (!parseValue(l.at(1), value)) continue;
