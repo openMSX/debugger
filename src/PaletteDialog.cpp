ab0362d1e4de9cbbe9bd4f7c079148a3510abf80
m9710797 <vermaelen.wouter@gmail.com>
2022-04-04 18:04:13 +0200

    Cleanups in PaletteDialog

--- src/PaletteDialog.cpp
+++ src/PaletteDialog.cpp
@@ -3,194 +3,174 @@
 #include <QGridLayout>
 #include <QPainter>
 #include "PaletteDialog.h"
-#include "ui_PaletteDialog.h"
 #include "Convert.h"
+#include <algorithm>
 
 
-PalettePatch::PalettePatch(QWidget *parent, int palnr) : QPushButton(parent),mycolor(Qt::green),isSelected(false)
+PalettePatch::PalettePatch(QWidget* parent, int palNr)
+    : QPushButton(parent), msxPalNr(palNr)
 {
-    msxpalnr=palnr;
 }
 
 //void PalettePatch::setColor(QRgb c)
 //{
-//    mycolor = c;
+//    myColor = c;
 //    update();
 //}
 
 void PalettePatch::updatePaletteChanged(const uint8_t* pal)
 {
-    int r = (pal[2 * msxpalnr + 0] & 0xf0) >> 4;
-    int b = (pal[2 * msxpalnr + 0] & 0x0f);
-    int g = (pal[2 * msxpalnr+ 1] & 0x0f);
-    r = (r >> 1) | (r << 2) | (r << 5);
-    b = (b >> 1) | (b << 2) | (b << 5);
-    g = (g >> 1) | (g << 2) | (g << 5);
-
-    mycolor = qRgb(r, g, b);
-    setSizePolicy(QSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding));
+    int r = (pal[2 * msxPalNr + 0] & 0xf0) >> 4;
+    int b = (pal[2 * msxPalNr + 0] & 0x0f);
+    int g = (pal[2 * msxPalNr + 1] & 0x0f);
+    auto scale = [](int x) { return (x >> 1) | (x << 2) | (x << 5); };
+    myColor = qRgb(scale(r), scale(g), scale(b));
+    setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding));
     update();
-//    printf("PalettePatch::updatePaletteChanged %i\n",msxpalnr);
+    //printf("PalettePatch::updatePaletteChanged %i\n", msxPalNr);
 }
 
-void PalettePatch::setHighlightTest(int colornr)
+void PalettePatch::setHighlightTest(int colorNr)
 {
-    if (isSelected == (colornr==msxpalnr)) return;
-
-    isSelected=(colornr==msxpalnr);
+    bool s = colorNr == msxPalNr;
+    if (isSelected == s) return;
+    isSelected = s;
     update();
 }
 
-
-void PalettePatch::paintEvent(QPaintEvent */*event*/)
+void PalettePatch::paintEvent(QPaintEvent* /*event*/)
 {
-    QPainter painter(this);
-        painter.setPen(isSelected?(Qt::white):QColor(mycolor));
-        painter.setBrush(QBrush(mycolor));
-        painter.drawRect(0,0,this->width()-1,this->height()-1);
+	QPainter painter(this);
+	painter.setPen(isSelected ? Qt::white : QColor(myColor));
+	painter.setBrush(QBrush(myColor));
+	painter.drawRect(0, 0, this->width() - 1, this->height() - 1);
 }
 
 
 
-PaletteDialog::PaletteDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::PaletteDialog),sourcepal(nullptr),currentcolor(0),autoSync(false)
+PaletteDialog::PaletteDialog(QWidget* parent)
+    : QDialog(parent), ui(std::make_unique<Ui::PaletteDialog>())
 {
     ui->setupUi(this);
     const QFont fixedFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
     ui->plainTextEdit->setFont(fixedFont);
 
     setWindowTitle("Palette editor");
-    memset(mypal,0,32);
-    memset(myoriginalpal,0,32);
-    //add colors to colorframe
+    std::fill(std::begin(myPal), std::end(myPal), 0);
+    std::fill(std::begin(myOriginalPal), std::end(myOriginalPal), 0);
+    // add colors to colorframe
 
     signalMapper = new QSignalMapper(this);
     auto* gridLayout = new QGridLayout;
     gridLayout->setSpacing(0);
     for (int i = 0; i < 16; ++i) {
-            auto* button = new PalettePatch(nullptr,i);
-            connect(button, SIGNAL(clicked(bool)), signalMapper,SLOT(map()));
-            signalMapper->setMapping(button, i);
-            gridLayout->addWidget(button, i/8, i%8);
-            connect(this,SIGNAL(paletteChanged(unsigned char*) ),
-                    button,SLOT(updatePaletteChanged(unsigned char*))
-                    );
-            connect(signalMapper, SIGNAL(mapped(int)),
-                    button, SLOT(setHighlightTest(int))
-                    );
-
-        }
-
+        auto* button = new PalettePatch(nullptr, i);
+        connect(button, SIGNAL(clicked(bool)), signalMapper, SLOT(map()));
+        signalMapper->setMapping(button, i);
+        gridLayout->addWidget(button, i / 8, i % 8);
+        connect(this, SIGNAL(paletteChanged(uint8_t*)),
+                button, SLOT(updatePaletteChanged(uint8_t*)));
         connect(signalMapper, SIGNAL(mapped(int)),
-                this, SLOT(colorSelected(int)));
+                button, SLOT(setHighlightTest(int)));
+    }
 
-        ui->colorsframe->setLayout(gridLayout);
-        connect(this,SIGNAL(paletteChanged(unsigned char*)),
-                this,SLOT(updateText()));
+    connect(signalMapper, SIGNAL(mapped(int)),
+            this, SLOT(colorSelected(int)));
+
+    ui->colorsframe->setLayout(gridLayout);
+    connect(this, SIGNAL(paletteChanged(uint8_t*)),
+            this, SLOT(updateText()));
 }
 
 void PaletteDialog::updateText()
 {
     ui->plainTextEdit->clear();
-    for (int i=0; i<4 ;i++){
+    for (int i = 0; i < 4; ++i) {
         QString txt(" db ");
-        for (int j=0; j<4 ;j++){
+        for (int j = 0; j < 4; ++j) {
             txt.append(QString("%1,%2 ")
-                        .arg(hexValue(mypal[2*(j+4*i)],2))
-                        .arg(hexValue(mypal[2*(j+4*i)+1],2))
-                    );
-            if (j<3){txt.append(',');};
+                .arg(hexValue(myPal[2 * (j + 4 * i) + 0], 2))
+                .arg(hexValue(myPal[2 * (j + 4 * i) + 1], 2)));
+            if (j < 3) txt.append(',');
         }
         ui->plainTextEdit->appendPlainText(txt);
-    };
+    }
 }
 
 void PaletteDialog::restoreOpeningsPalette()
 {
-    memcpy(mypal,myoriginalpal,32);
-    emit paletteChanged(mypal); //resets the PalettePatches
-    colorSelected(currentcolor); //resets the sliders
-    if (autoSync){
+    memcpy(myPal, myOriginalPal, sizeof(myPal));
+    emit paletteChanged(myPal); // Resets the PalettePatches
+    colorSelected(currentColor); // Resets the sliders
+    if (autoSync) {
         syncToSource();
     }
 }
 
-PaletteDialog::~PaletteDialog()
+void PaletteDialog::colorSelected(int colorNumber)
 {
-    delete ui;
-}
-
-void PaletteDialog::colorSelected(int colornumber)
-{
-    currentcolor=colornumber;
-    int r = (mypal[2 * currentcolor + 0] & 0xf0) >> 4;
-    int b = (mypal[2 * currentcolor + 0] & 0x0f);
-    int g = (mypal[2 * currentcolor + 1] & 0x0f);
+    currentColor = colorNumber;
+    int r = (myPal[2 * currentColor + 0] & 0xf0) >> 4;
+    int b = (myPal[2 * currentColor + 0] & 0x0f);
+    int g = (myPal[2 * currentColor + 1] & 0x0f);
     ui->horizontalSlider_R->setValue(r);
     ui->horizontalSlider_G->setValue(g);
     ui->horizontalSlider_B->setValue(b);
-    ui->label_colornr->setText(QString("Color %1").arg(colornumber));
+    ui->label_colornr->setText(QString("Color %1").arg(colorNumber));
 }
 
-void PaletteDialog::setPalette(unsigned char *pal)
+void PaletteDialog::setPalette(uint8_t* pal)
 {
-    sourcepal=pal;
-    memcpy(myoriginalpal,pal,32);
-    memcpy(mypal,pal,32);
-    emit paletteChanged(mypal);
+    sourcePal = pal;
+    memcpy(myOriginalPal, pal, sizeof(myOriginalPal));
+    memcpy(myPal, pal, sizeof(myPal));
+    emit paletteChanged(myPal);
 }
 
-unsigned char* PaletteDialog::getPalette()
+uint8_t* PaletteDialog::getPalette()
 {
-    return mypal;
+    return myPal;
 }
 
 //void PaletteDialog::decodepalette()
 //{
 //    for (int i = 0; i < 16; ++i) {
-//        int r = (mypal[2 * i + 0] & 0xf0) >> 4;
-//        int b = (mypal[2 * i + 0] & 0x0f);
-//        int g = (mypal[2 * i + 1] & 0x0f);
-
-//        r = (r >> 1) | (r << 2) | (r << 5);
-//        b = (b >> 1) | (b << 2) | (b << 5);
-//        g = (g >> 1) | (g << 2) | (g << 5);
-
-//        msxpallet[i] = qRgb(r, g, b);
-
-//        QGridLayout* l=dynamic_cast<QGridLayout*>(ui->colorsframe->layout());
-//        dynamic_cast<QPushButton*>(l->itemAtPosition(i%8,i/8)->widget())->setText(QString("%1(%2%3%4)").arg(i).arg(r).arg(g).arg(b));
-
+//        int r = (myPal[2 * i + 0] & 0xf0) >> 4;
+//        int b = (myPal[2 * i + 0] & 0x0f);
+//        int g = (myPal[2 * i + 1] & 0x0f);
+//
+//        auto scale = [](int x) { return (x >> 1) | (x << 2) | (x << 5); };
+//        msxPalette[i] = qRgb(scale(r), scale(g), scale(b));
+//
+//        QGridLayout* l = dynamic_cast<QGridLayout*>(ui->colorsframe->layout());
+//        dynamic_cast<QPushButton*>(l->itemAtPosition(i % 8, i / 8)->widget())->setText(QString("%1(%2%3%4)").arg(i).arg(r).arg(g).arg(b));
 //    }
 //}
 
 void PaletteDialog::combineRGB()
 {
-    int r=ui->horizontalSlider_R->value();
-    int g=ui->horizontalSlider_G->value();
-    int b=ui->horizontalSlider_B->value();
-    mypal[currentcolor*2]=r*16+b;
-    mypal[currentcolor*2+1]=g;
-    emit paletteChanged(mypal);
-    if (autoSync){
+    int r = ui->horizontalSlider_R->value();
+    int g = ui->horizontalSlider_G->value();
+    int b = ui->horizontalSlider_B->value();
+    myPal[2 * currentColor + 0] = 16 * r + b;
+    myPal[2 * currentColor + 1] = g;
+    emit paletteChanged(myPal);
+    if (autoSync) {
         syncToSource();
     }
 }
 
 void PaletteDialog::syncToSource()
 {
-    memcpy(sourcepal,mypal,32);
+    memcpy(sourcePal, myPal, sizeof(myPal));
     emit paletteSynced();
 }
 
 void PaletteDialog::setAutoSync(bool value)
 {
-    if (autoSync==value)return;
-
+    if (autoSync == value) return;
     autoSync = value;
     ui->cb_autosync->setChecked(value);
-
 }
 
 void PaletteDialog::on_horizontalSlider_R_valueChanged(int value)
@@ -211,23 +191,21 @@ void PaletteDialog::on_horizontalSlider_B_valueChanged(int value)
     combineRGB();
 }
 
-
-void PaletteDialog::on_buttonBox_clicked(QAbstractButton *button)
+void PaletteDialog::on_buttonBox_clicked(QAbstractButton* button)
 {
     if (button== ui->buttonBox->button(QDialogButtonBox::Apply) ||
-        button== ui->buttonBox->button(QDialogButtonBox::Ok)){
+        button== ui->buttonBox->button(QDialogButtonBox::Ok)) {
         syncToSource();
-    } else
-    if (button== ui->buttonBox->button(QDialogButtonBox::Reset) ||
-        button== ui->buttonBox->button(QDialogButtonBox::Cancel)){
+    } else if (button== ui->buttonBox->button(QDialogButtonBox::Reset) ||
+               button== ui->buttonBox->button(QDialogButtonBox::Cancel)) {
         restoreOpeningsPalette();
     }
 }
 
 void PaletteDialog::on_cb_autosync_stateChanged(int arg1)
 {
-    autoSync=(arg1 !=Qt::Unchecked);
-    if (autoSync){
+    autoSync = arg1 != Qt::Unchecked;
+    if (autoSync) {
         syncToSource();
-    };
+    }
 }
--- src/PaletteDialog.h
+++ src/PaletteDialog.h
@@ -1,35 +1,34 @@
 #ifndef PALETTEDIALOG_H
 #define PALETTEDIALOG_H
 
+#include "ui_PaletteDialog.h"
 #include <QDialog>
 #include <QSignalMapper>
 #include <QPushButton>
 #include <cstdint>
-
+#include <memory>
 
 namespace Ui {
-class PaletteDialog;
+    class PaletteDialog;
 }
 
 class PalettePatch: public QPushButton
 {
     Q_OBJECT
 public:
-    explicit PalettePatch(QWidget *parent = nullptr,int palnr=0);
-//    ~PalettePatch();
-//    void setColor(QRgb c);
+    explicit PalettePatch(QWidget* parent = nullptr, int palNr = 0);
+    //void setColor(QRgb c);
 public slots:
     void updatePaletteChanged(const uint8_t* pal);
-    void setHighlightTest(int colornr);
+    void setHighlightTest(int colorNr);
 
 protected:
-    void paintEvent(QPaintEvent * event) override;
+    void paintEvent(QPaintEvent* event) override;
 
 private:
-    QRgb mycolor;
-    bool isSelected;
-    int msxpalnr;
-
+    QRgb myColor = Qt::green;
+    bool isSelected = false;
+    int msxPalNr;
 };
 
 
@@ -38,46 +37,44 @@ class PaletteDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit PaletteDialog(QWidget *parent = nullptr);
-    ~PaletteDialog();
+    explicit PaletteDialog(QWidget* parent = nullptr);
 
-    void setPalette(unsigned char *pal);
-    unsigned char* getPalette();
+    void setPalette(uint8_t* pal);
+    uint8_t* getPalette();
     void syncToSource();
     void setAutoSync(bool value);
 
 signals:
-    void paletteChanged(unsigned char* pal);
+    void paletteChanged(uint8_t* pal);
     void paletteSynced();
 
 private slots:
-    void colorSelected(int colornumber);
+    void colorSelected(int colorNumber);
     void on_horizontalSlider_R_valueChanged(int value);
     void on_horizontalSlider_G_valueChanged(int value);
     void on_horizontalSlider_B_valueChanged(int value);
     void restoreOpeningsPalette();
 
-    void on_buttonBox_clicked(QAbstractButton *button);
+    void on_buttonBox_clicked(QAbstractButton* button);
 
     void on_cb_autosync_stateChanged(int arg1);
 
     void updateText();
 
-
 private:
-    Ui::PaletteDialog *ui;
-    QSignalMapper *signalMapper;
-//    void decodepalette();
+    std::unique_ptr<Ui::PaletteDialog> ui;
+    QSignalMapper* signalMapper;
+    //void decodepalette();
     void combineRGB();
 
-    unsigned char* sourcepal;
-    unsigned char mypal[32];
-    unsigned char myoriginalpal[32];
+    uint8_t* sourcePal = nullptr;
+    uint8_t myPal[32];
+    uint8_t myOriginalPal[32];
 
-    int currentcolor;
-    QRgb msxpallet[16];
+    int currentColor = 0;
+    QRgb msxPalette[16];
 
-    bool autoSync;
+    bool autoSync = false;
 };
 
 #endif // PALETTEDIALOG_H
